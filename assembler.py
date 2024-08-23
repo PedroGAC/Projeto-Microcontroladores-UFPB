960aa5976313a4ab4cc6e6a7d8f9d82fcb8f5897
PedroGAC <pedro.cruz@estudante.cear.ufpb.br>
2024-08-22 21:03:47 -0300

    Atualiza assembler

--- assembler.py
+++ assembler.py
@@ -1,28 +1,27 @@
 def assemble(instruction):
-    # Split instruction into its components
     parts = instruction.split()
     opcode = parts[0]
     
     if opcode == 'ADD':
         func7 = '0000000'
-        regDest = format(int(parts[1][1:]), '05b')  # R1 -> regDest
-        reg2 = format(int(parts[2][1:]), '05b')     # R2 -> reg2
-        reg1 = format(int(parts[3][1:]), '05b')     # R3 -> reg1
+        regDest = format(int(parts[1][1:]), '05b')  
+        reg2 = format(int(parts[2][1:]), '05b')     
+        reg1 = format(int(parts[3][1:]), '05b')     
         func3 = '000'
         opcode_bin = '0110011'
         return f'{func7}{reg1}{reg2}{func3}{regDest}{opcode_bin}'
     
     elif opcode == 'SUB':
         func7 = '0100000'
-        regDest = format(int(parts[1][1:]), '05b')  # R1 -> regDest
-        reg2 = format(int(parts[2][1:]), '05b')     # R2 -> reg2
-        reg1 = format(int(parts[3][1:]), '05b')     # R3 -> reg1
+        regDest = format(int(parts[1][1:]), '05b')  
+        reg2 = format(int(parts[2][1:]), '05b')     
+        reg1 = format(int(parts[3][1:]), '05b')     
         func3 = '000'
         opcode_bin = '0110011'
         return f'{func7}{reg1}{reg2}{func3}{regDest}{opcode_bin}'
     
     elif opcode == 'ADDi':
-        imediato = format(int(parts[2]), '012b')  # 12-bit immediate value
+        imediato = format(int(parts[2]), '012b')  
         reg = format(int(parts[1][1:]), '05b')
         func3 = '000'
         regDest = format(int(parts[3][1:]), '05b')
@@ -30,7 +29,7 @@ def assemble(instruction):
         return f'{imediato}{reg}{func3}{regDest}{opcode_bin}'
     
     elif opcode == 'SUBi':
-        imediato = format(int(parts[2]), '012b')  # 12-bit immediate value
+        imediato = format(int(parts[2]), '012b')  
         reg = format(int(parts[1][1:]), '05b')
         func3 = '000'
         regDest = format(int(parts[3][1:]), '05b')
@@ -47,20 +46,17 @@ def assemble(instruction):
         return f'{imm_part1}{reg1}{reg2}{func3}{imm_part2}{opcode_bin}'
     
     elif opcode == 'JMP':
-        # Extrai o deslocamento e converte para binário com 6 bits
-        offset = format(int(parts[1]), '06b')  # 6-bit offset
-        opcode_bin = '1101111'  # Opcode de 7 bits
-        zeros = '0' * 19  # 19 bits de zeros
-        
-        # Combina tudo em uma instrução de 32 bits
+        offset = format(int(parts[1]), '06b') # 6-bit offset
+        opcode_bin = '1101111' 
+        zeros = '0' * 19  
         return f'{offset}{zeros}{opcode_bin}'
 
     else:
         raise ValueError("Instrução desconhecida!")
 
-# Lista de instruções para serem convertidas
 instructions = [
-    "ADDi R1 5 R1"
+    "ADDi R1 5 R1",
+    "ADDi R1 5 R1",
     "ADDi R2 4 R0",
     "SUB R4 R5 R6",
     "ADDi R7 3 R8",
@@ -78,6 +74,6 @@ with open('output_instructions.txt', 'w') as f:
     # Preenche as linhas restantes com zeros, se necessário
     remaining_lines = 64 - len(instructions)
     for _ in range(remaining_lines):
-        f.write('0' * 32 + '\n')  # Preenche com 32 zeros
-
+        f.write('0' * 32 + '\n')
+        
 print("Instruções montadas e salvas em 'output_instructions.txt'")
